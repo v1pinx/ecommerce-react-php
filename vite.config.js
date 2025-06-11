a43211b6bc6ce92cdfbfd39b0797ca709b1ca83c
v1pinx <vkprajapati665@gmail.com>
2025-06-11 14:57:55 +0530

    Minor changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-})
+  base: "/",
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://20.244.105.42",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
